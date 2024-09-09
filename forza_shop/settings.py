7101a9859e03dec67c13407cce3ffecbd07122a3
Ignasius Bramantya Widiprasetya <ignasius.bramantya@gmail.com>
2024-09-09 21:17:00 +0700

    fixing

--- forza_shop/settings.py
+++ forza_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bfrm+lmv&nkkkm#m5%*9foywj7&_agxcn^^$5u&9n%kspl8z$$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ignasius-bramantya-forzashops.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ignasius-bramantya-forzashops.pbp.cs.ui.ac.id/"]
 
 
 # Application definition
