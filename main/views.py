9363ec0d89d92a66ffca2981b35b6e4b678518e1
Ignasius Bramantya Widiprasetya <ignasius.bramantya@gmail.com>
2024-09-09 18:31:50 +0700

    deploy to pws

--- main/views.py
+++ main/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
 
+def show_main(request):
+    context = {
+        'name' : 'Final Fantasy V',
+        'price': '600000',
+        'description': 'Adventure Video Games'
+    }
+
+    return render(request, "main.html", context)
