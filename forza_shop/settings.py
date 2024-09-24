e3df2f76a9e19e8f531fcc9d781adc6a4312b8ae
Ignasius Bramantya Widiprasetya <ignasius.bramantya@gmail.com>
2024-09-25 01:53:52 +0700

    finishing code tugas 4 tapi blm readme

--- forza_shop/settings.py
+++ forza_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bfrm+lmv&nkkkm#m5%*9foywj7&_agxcn^^$5u&9n%kspl8z$$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ignasius-bramantya-forzashops.pbp.cs.ui.ac.id"]
 
